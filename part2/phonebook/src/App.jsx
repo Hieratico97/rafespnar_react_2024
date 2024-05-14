65ff5348a2ef750fc1138274fa415e66df2a9755
Hieratico97 <rafael.espigares.naranjo@gmail.com>
2024-05-14 18:35:47 +0200

    2.8
    completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,38 +2,57 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+     number: '635888234'
+  }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
   const addName = (event) => {
    
     
     event.preventDefault()
-    const newPerson ={name: newName}
+    
     if(nameExists(newName)){
       alert (newName+ " is already added to phonebook")
     }
+    if(nameExists(newNumber)){
+      alert (newNumber+ " is already added to phonebook")
+    }
+    const newPerson ={name: newName, number:newNumber} 
     setPersons([...persons,newPerson])
     setNewName('')
+    setNewNumber('')
    
     }
+    
     
     const nameExists = (check) => {
       return persons.find(person => person.name === check)
     }
-   
+   --
   
 
   return (
     <div>
+      
       <h2>Phonebook</h2>
+      <div>
+      filter show with <input value={search} onChange={handleSearch}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -42,7 +61,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul style={{ listStyleType: 'none', padding: 0 }}>
         {persons.map((person,index) => 
-          <li key={index}>{person.name}</li>
+          <li key={index}>{person.name} {person.number}</li>
         )}
 
 
