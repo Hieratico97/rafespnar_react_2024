02f114e3bedac7292e2abfc7fd2dc91baf4f2b14
Hieratico97 <rafael.espigares.naranjo@gmail.com>
2024-06-06 13:56:30 +0200

    3.13 .14 .15 .16 .17. 18
    completed

--- part3/index.js
+++ part3/index.js
@@ -16,14 +16,27 @@ app.use(morgan('tiny'))
 app.use(morgan(':method  :url  :status :res[content-length]- :response-time ms :content'))
 app.use(express.static('dist'))
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
+
 morgan.token('content', (request, response) => {
   if (request.method === 'POST') {
     return JSON.stringify(request.body)
   } 
   return ''
 })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// este debe ser el último middleware cargado, ¡también todas las rutas deben ser registrada antes que esto!
+app.use(errorHandler)
 
 
 
@@ -35,8 +48,8 @@ app.get('/api/persons', (request, response) => {
 })
 app.get('/info', (request, response) => {
   Person.find({}).then(persons => {
-  const maxId = person.length > 0
-  ? Math.max(...Person.map(n => n.id)) 
+  const maxId = persons.length > 0
+  ? Math.max(...persons.map(n => n.id)) 
   : 0
   const requestTime = new Date().toLocaleString()
   response.send(`
@@ -48,14 +61,14 @@ app.get('/info', (request, response) => {
 })
 })
 app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
+  Person.findById(request.params.id).then(person =>{
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
   })
-})
-app.delete('/api/persons/:id', (request, response) => {
-  Person.findByIdAndDelete(request.params.id).then(person => {
-    response.json(person)
-  })
+  .catch(error => next(error))
 })
 
 
@@ -64,7 +77,7 @@ app.post('/api/persons', (request, response) => {
 
   if (!body.name || !body.number) {
     return response.status(400).json({ error: 'content missing' })
-  }
+  } 
   
 
 
@@ -78,6 +91,20 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 
+})
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 const PORT = process.env.PORT
 app.listen(PORT, () => {
