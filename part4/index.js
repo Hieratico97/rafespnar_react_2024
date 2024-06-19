f82f34f57fd40d66a679893c85a7b26368004612
Hieratico97 <rafael.espigares.naranjo@gmail.com>
2024-06-19 08:53:32 +0200

    4.2
    test

--- part4/index.js
+++ part4/index.js
@@ -1,52 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app') 
+const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGO_URL
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  
-  Blog
-    .find({})
-    .then(blogs => {
-      
-      response.json(blogs)
-      
-     
-}).catch(error => {
-  console.error('Error fetching blogs:', error.message)
-  response.status(500).json({ error: 'Internal server error' })
-})  
-    })
-    
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-  console.log("hola")
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    console.log(response)
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
